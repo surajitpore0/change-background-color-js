2eae52dc604d8913979c8d8d70993dc3eacdf359
PoreXD <surajitpore0@gmail.com>
2021-04-07 17:06:38 +0530

    initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const colors = ["yellow", "green", "blue", "#f15025", "rgba(125,125,0.5)"];
+
+const btn = document.getElementById("btn");
+
+// add event listner
+btn.addEventListener("click", function () {
+    let random = Math.floor(Math.random() * colors.length);
+
+    const body = document.body;
+    body.style.backgroundColor = colors[random];
+    console.log(random);
+});
